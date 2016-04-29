590acd1475c0b4c3dd2b0cf36013d456a4b213af
EstebanButti <ebutti@solstice-consulting.com>
2016-04-29 18:46:48 -0300

    Upgrades in SyncFragment. Added menu to download/delete properties selected.

--- app/src/main/java/lens/inmo360/views/SyncFragment.java
+++ app/src/main/java/lens/inmo360/views/SyncFragment.java
@@ -1,9 +1,11 @@
 package lens.inmo360.views;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,10 +13,24 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.github.clans.fab.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import lens.inmo360.R;
+import lens.inmo360.adapters.BasePropertyAdapter;
+import lens.inmo360.managers.HttpManager;
 import lens.inmo360.managers.SyncManager;
 import lens.inmo360.model.Property;
+import lens.inmo360.model.PropertyAPIInterface;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by estebanbutti on 4/26/16.
@@ -22,7 +38,15 @@ import lens.inmo360.model.Property;
 public class SyncFragment extends android.support.v4.app.Fragment {
 
 //    @BindView(R.id.propertyBaseList)
-//    ListView propertyBaseList;
+    ListView propertyBaseList;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private FloatingActionButton downloadFloatingButton;
+    private FloatingActionButton deleteFloatingButton;
+    BasePropertyAdapter adapter;
+    MaterialDialog loadingDialog;
+    SyncManager mSyncManager = new SyncManager();
 
     @Nullable
     @Override
@@ -32,107 +56,143 @@ public class SyncFragment extends android.support.v4.app.Fragment {
 //        ButterKnife.bind(this, view);
         setHasOptionsMenu(true);
 
-//        final ListView propertyBaseList = (ListView) view.findViewById(R.id.propertyBaseList);
-//
-//        HttpManager httpManager = HttpManager.getInstance(getActivity().getApplicationContext());
-//
-//        PropertyAPIInterface apiService =
-//                httpManager.getRetrofit().create(PropertyAPIInterface.class);
-//
-//        Call<List<Property>> call = apiService.getAllPropertiesForCompany("1");
-//
-//        call.enqueue(new Callback<List<Property>>() {
+//        propertyBaseList = (ListView) view.findViewById(R.id.propertyBaseList);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.propertyBaseList);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        loadingDialog = new MaterialDialog.Builder(getActivity())
+                .title(R.string.downloading)
+                .content(R.string.please_wait)
+                .progress(true, 0)
+                .progressIndeterminateStyle(true)
+                .build();
+        loadingDialog.show();
+
+        HttpManager httpManager = HttpManager.getInstance();
+        PropertyAPIInterface apiService =
+                httpManager.getRetrofit().create(PropertyAPIInterface.class);
+
+        Call<List<Property>> call = apiService.getAllPropertiesForCompany("1");
+
+        call.enqueue(new Callback<List<Property>>() {
+            @Override
+            public void onResponse(Call<List<Property>> call, Response<List<Property>> response) {
+                int statusCode = response.code();
+                ArrayList<Property> properties = (ArrayList<Property>) response.body();
+
+                // create an Object for Adapter
+                mAdapter = new BasePropertyAdapter(properties);
+
+                // set the adapter object to the Recyclerview
+                mRecyclerView.setAdapter(mAdapter);
+
+                loadingDialog.dismiss();
+            }
+
+            @Override
+            public void onFailure(Call<List<Property>> call, Throwable t) {
+                // Log error here since request failed
+                Log.d("Error en call", call.toString());
+            }
+        });
+
+        downloadFloatingButton = (FloatingActionButton) view.findViewById(R.id.downloadFloatingButton);
+        downloadFloatingButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                new MaterialDialog.Builder(getActivity())
+                        .title(R.string.syncDialogTitle)
+                        .content(R.string.downloadConfirmationContent)
+                        .positiveText(R.string.downloadConfirmationPositiveButton)
+                        .negativeText(R.string.cancelNegativeButton)
+                .onPositive(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                        ArrayList<Property> selectedProperties = new ArrayList<>();
+                        ArrayList<Property> properties = ((BasePropertyAdapter) mAdapter)
+                                .getProperties();
+
+                        for (int i = 0; i < properties.size(); i++) {
+                            Property property = properties.get(i);
+                            if (property.isDownloaded()) {
+                                selectedProperties.add(property);
+                            }
+                        }
+
+                        mSyncManager.downloadProperties(getActivity(), selectedProperties);
+                    }
+                })
+                .onNegative(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                        dialog.dismiss();
+                    }
+                }).show();
+            }
+        });
+
+        deleteFloatingButton = (FloatingActionButton) view.findViewById(R.id.deleteFloatingButton);
+        deleteFloatingButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                new MaterialDialog.Builder(getActivity())
+                        .title(R.string.syncDialogTitle)
+                        .content(R.string.deleteConfirmationContent)
+                        .positiveText(R.string.deleteConfirmationPositiveButton)
+                        .negativeText(R.string.cancelNegativeButton)
+                        .onPositive(new MaterialDialog.SingleButtonCallback() {
+                            @Override
+                            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                ArrayList<Property> selectedProperties = new ArrayList<>();
+                                List<Property> properties = ((BasePropertyAdapter) mAdapter)
+                                        .getProperties();
+
+                                for (int i = 0; i < properties.size(); i++) {
+                                    Property property = properties.get(i);
+                                    if (property.isDownloaded()) {
+                                        selectedProperties.add(property);
+                                    }
+                                }
+
+                                mSyncManager.deleteProperties(getActivity(), selectedProperties);
+                            }
+                        })
+                        .onNegative(new MaterialDialog.SingleButtonCallback() {
+                            @Override
+                            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                dialog.dismiss();
+                            }
+                        }).show();
+            }
+        });
+
+        // When item is tapped, toggle checked properties of CheckBox and Property.
+//        propertyBaseList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+//        {
 //            @Override
-//            public void onResponse(Call<List<Property>> call, Response<List<Property>> response) {
-//                int statusCode = response.code();
-//                List<Property> properties = response.body();
-//
-//                final BasePropertyAdapter adapter = new BasePropertyAdapter(getActivity().getApplicationContext(), properties);
-//                propertyBaseList.setAdapter(adapter);
-//
-//                // TEST DOWNLOADING IMAGE
-//                DownloadImageTask task = new DownloadImageTask();
-//                task.execute(properties.get(1));
-//
-//                propertyBaseList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//
-//                    @Override
-//                    public void onItemClick(AdapterView<?> parent, final View view,
-//                                            int position, long id) {
-//                        //                        final String item = (String) parent.getItemAtPosition(position);
-//                        //                        view.animate().setDuration(2000).alpha(0)
-//                        //                                .withEndAction(new Runnable() {
-//                        //                                    @Override
-//                        //                                    public void run() {
-//                        //                                        list.remove(item);
-//                        //                                        adapter.notifyDataSetChanged();
-//                        //                                        view.setAlpha(1);
-//                        //                                    }
-//                        //                                });
-//                    }
-//                });
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<Property>> call, Throwable t) {
-//                // Log error here since request failed
-//                Log.d("Error en call", call.toString());
+//            public void onItemClick(AdapterView<?> parent, View item,
+//                                    int position, long id)
+//            {
+//                Property property = adapter.getItem(position);
+//                property.toggleIsDownloaded();
+//                BasePropertyViewHolder viewHolder = (BasePropertyViewHolder) item
+//                        .getTag();
+//                viewHolder.getIsDownloadedCheckbox().setChecked(property.isDownloaded());
 //            }
 //        });
 
         return view;
     }
-
-//    @Override
-//    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-//        super.onViewCreated(view, savedInstanceState);
-
-//        HttpManager httpManager = HttpManager.getInstance(getActivity().getApplicationContext());
-//
-//        PropertyAPIInterface apiService =
-//                httpManager.getRetrofit().create(PropertyAPIInterface.class);
-//
-//        Call<List<Property>> call = apiService.getAllPropertiesForCompany("1");
-//
-//        call.enqueue(new Callback<List<Property>>() {
-//            @Override
-//            public void onResponse(Call<List<Property>> call, Response<List<Property>> response) {
-//                int statusCode = response.code();
-//                List<Property> properties = response.body();
-//
-//                final BasePropertyAdapter adapter = new BasePropertyAdapter(getActivity().getApplicationContext(), properties);
-//                propertyBaseList.setAdapter(adapter);
-//
-//                // TEST DOWNLOADING IMAGE
-//                DownloadImageTask task = new DownloadImageTask();
-//                task.execute(properties.get(1));
-//
-//                propertyBaseList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//
-//                    @Override
-//                    public void onItemClick(AdapterView<?> parent, final View view,
-//                                            int position, long id) {
-//                        //                        final String item = (String) parent.getItemAtPosition(position);
-//                        //                        view.animate().setDuration(2000).alpha(0)
-//                        //                                .withEndAction(new Runnable() {
-//                        //                                    @Override
-//                        //                                    public void run() {
-//                        //                                        list.remove(item);
-//                        //                                        adapter.notifyDataSetChanged();
-//                        //                                        view.setAlpha(1);
-//                        //                                    }
-//                        //                                });
-//                    }
-//                });
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<Property>> call, Throwable t) {
-//                // Log error here since request failed
-//                Log.d("Error en call", call.toString());
-//            }
-//        });
-//    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -162,7 +222,6 @@ public class SyncFragment extends android.support.v4.app.Fragment {
 
             //SYNC ALL
 
-
             return true;
         }
 
@@ -171,30 +230,6 @@ public class SyncFragment extends android.support.v4.app.Fragment {
 
     public boolean isExternalStorageWritable() {
         String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            return true;
-        }
-        return false;
-    }
-
-    private class DownloadImageTask extends AsyncTask<Property, Void,
-            Boolean> {
-        SyncManager syncManager;
-
-        @Override
-        protected void onPreExecute() {
-            syncManager = new SyncManager();
-        }
-
-        @Override
-        protected Boolean doInBackground(Property... params) {
-            return syncManager.downloadPropertyImage(getActivity().getApplicationContext(),
-                    params[0].getImages().get(1), params[0].getAddress());
-        }
-
-        @Override
-        protected void onPostExecute(Boolean success) {
-            Log.d("Downloaded file: ", success.toString());
-        }
+        return Environment.MEDIA_MOUNTED.equals(state);
     }
 }
