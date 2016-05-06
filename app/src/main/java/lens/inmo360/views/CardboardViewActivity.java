9122fbf7feee8802033e71bd8301315248bae02d
EstebanButti <ebutti@solstice-consulting.com>
2016-05-06 12:18:55 -0300

    House cards shown on mainFragment using images from downloaded houses.

--- app/src/main/java/lens/inmo360/views/CardboardViewActivity.java
+++ app/src/main/java/lens/inmo360/views/CardboardViewActivity.java
@@ -203,7 +203,7 @@ public class CardboardViewActivity extends CardboardActivity implements Cardboar
 
         glViewport(0, 0, width, height);
         /** Setting the projection Matrix for the view **/
-        Matrix.perspectiveM(mProjectionMatrix2,0, 90, (float) width
+        Matrix.perspectiveM(mProjectionMatrix2,0, 100, (float) width
                 / (float) height, 1f, 10f);
 
     }
